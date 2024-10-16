85eafe67f081c74de92dfbee8357cc948d921bdf
Brent Bumann <brent.bumann@aol.com>
2024-10-16 14:46:54 -0700

    Add loading spinner for restaurants list

--- RestaurantList.swift
+++ RestaurantList.swift
@@ -8,49 +8,64 @@
 import SwiftUI
 
 struct RestaurantList: View {
-    var restaurants: [Restaurant] = []
+    @Binding var restaurants: [Restaurant]
     
     var body: some View {
-        ScrollView {
-            ForEach(restaurants) { restaurant in
-                HStack(alignment: .top) {
-                    AsyncImage(url: URL(string: restaurant.thumbnail), content: { image in
-                        image
-                            .resizable()
-                            .frame(height: 150)
-                            .aspectRatio(contentMode: .fit)
-                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                        }, placeholder: {
-                            ProgressView()
-                        }
-                    )
-                    .padding(.trailing, 10)
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(restaurant.name)
-                            .font(.headline)
-                            
-                        
-                        Text("restaurant address")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                        
-                        Text("Rating: \(4.5, specifier: "%.1f")") // Display rating
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                    .padding(.vertical, 8)
+        VStack {
+            if restaurants.isEmpty {
+                VStack {
+                    ProgressView()
+                    Text("Searching for nearby restaurants...")
+                        .font(.headline)
+                        .padding(.top, 10)
                 }
-                .padding(.trailing, 10.0)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color(.systemBackground))
-                )
+                .padding()
+            } else {
+                ScrollView {
+                    ForEach(restaurants) { restaurant in
+                        HStack(alignment: .top) {
+                            AsyncImage(url: URL(string: restaurant.thumbnail), content: { image in
+                                image
+                                    .resizable()
+                                    .frame(height: 150)
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            }, placeholder: {
+                                ProgressView()
+                            }
+                            )
+                            .padding(.trailing, 10)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(restaurant.name)
+                                    .font(.headline)
+                                
+                                
+                                Text("restaurant address")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                
+                                Text("Rating: \(4.5, specifier: "%.1f")") // Display rating
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
+                            .padding(.vertical, 8)
+                        }
+                        .padding(.trailing, 10.0)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(.systemBackground))
+                        )
+                    }
+                }
             }
+            
         }
     }
 }
 
+var restaurants: [Restaurant] = mockRestaurantList
+var empty: [Restaurant] = []
 #Preview {
-    RestaurantList(restaurants: mockRestaurantList)
+    RestaurantList(restaurants: .constant(restaurants))
 }
